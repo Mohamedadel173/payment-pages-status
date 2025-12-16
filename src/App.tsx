4decc52086e28f6e6ce967add7dd038bee5c68ed
DULA .JR <mohamedadelq17@gmail.com>
2025-12-16 21:17:11 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PaymentSuccess from "./PaymentSuccess";
-import PaymentCancelled from "./PaymentCancelled";
-import Home from "./Home";
+// import PaymentCancelled from "./PaymentCancelled";
+// import Home from "./Home";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/payment-success" element={<PaymentSuccess />} />
-        <Route path="/payment-cancelled" element={<PaymentCancelled />} />
+        <Route path="/" element={<PaymentSuccess />} />
+        {/* <Route path="/payment-success" element={<PaymentSuccess />} /> */}
+        {/* <Route path="/payment-cancelled" element={<PaymentCancelled />} /> */}
       </Routes>
     </Router>
   );
